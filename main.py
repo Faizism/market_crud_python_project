86fc2d0354357a3f024741ee4d49b44393732b89
Faiz <ismail1.faiz1@gmail.com>
2024-10-07 17:10:38 +0700

    Add Validation Stock

--- main.py
+++ main.py
@@ -6,13 +6,43 @@ price_apple = 10_000
 price_orange = 15_000
 price_grape = 20_000
 
-# user menginput jumlah belanjaan
+# menentukan stok buah
+stock_apple = 10
+stock_orange = 7
+stock_grape = 6
+
+# user menginput jumlah belanjaan untuk buah apel
 amount_apple = input("Masukkan Jumlah Apel : ")
 amount_apple = int(amount_apple)
+
+# validasi stok apel
+while amount_apple > stock_apple:
+    print("Jumlah yang dimasukkan terlalu banyak")
+    print(f"Stock Apel tinggal: {stock_apple}")
+    amount_apple = input("Masukkan Jumlah Apel : ")
+    amount_apple = int(amount_apple)
+
+# user menginput jumlah belanjaan untuk buah jeruk
 amount_orange = input("Masukkan Jumlah Jeruk : ")
 amount_orange = int(amount_orange)
+
+# validasi stok jeruk
+while amount_orange>stock_orange :
+    print("Jumlah yang dimasukkan terlalu banyak")
+    print(f"Stock Jeruk tinggal: {stock_orange}")
+    amount_orange = input("Masukkan Jumlah Jeruk : ")
+    amount_orange = int(amount_orange)
+
+# user menginput jumlah belanjaan untuk buah anggur
 amount_grape = input("Masukkan Jumlah Anggur : ") 
 amount_grape = int(amount_grape)
+
+# validasi stok anggur
+while amount_grape>stock_grape:
+    print("Jumlah yang dimasukkan terlalu banyak")
+    print(f"Stock Anggur tinggal: {stock_grape}")
+    amount_grape = input("Masukkan Jumlah Anggur : ") 
+    amount_grape = int(amount_grape)
 
 # perhitungan total harga yang akan dibayarkan
 print("\nDetail Belanja\n")
@@ -30,14 +60,18 @@ print(f"\nTotal : {total_cost}\n")
 
 ### JCDS 0412 - 03/10/2024
 ### Exercise 2
+# user menginput jumlah uang yang akan dikeluarkan
 amount_money = input("Masukkan jumlah uang : ")
 amount_money = int(amount_money)
 
-if(amount_money < total_cost):
+# validasi uang dari user
+while(amount_money < total_cost):
    print("Transaksi anda dibatalkan")
    short = (total_cost - amount_money)
    print(f"uangnya kurang sebesar {short}")
-elif (amount_money == total_cost):
+   amount_money = input("Masukkan jumlah uang : ")
+   amount_money = int(amount_money)
+if (amount_money == total_cost):
    print("Terimakasih")
 else :
    print("Terimakasih\n")
